aae8045bce037722a92d2097b8de68fe6c6e8a17
Sizzad-Hosen <sizzadhosenofficial@gmail.com>
2025-12-11 17:36:52 +0600

    user,category,comment controller create

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+        public function register(Request $request)
+    {
+        return response()->json(['message' => 'Hello world']);
+    }
+
+    public function login(Request $request)
+    {
+        return response()->json(['message' => 'Logged in']);
+    }
+ 
+    // public function register(Request $request){
+    //     $data = $request->validate([
+    //         'name'=>'required|string|max:255',
+    //         'email'=>'required|email|unique:users',
+    //         'password'=>'required|string|min:6',
+    //         'role'=>'in:admin,user'
+    //     ]);
+
+    //     $data['password'] = bcrypt($data['password']);
+
+    //     $user = User::create($data);
+
+    //     // // Create token
+    //     // $token = $user->createToken('api_token')->plainTextToken;
+
+    //       return response()->json([
+    //         'message' => 'User registered successfully',
+    //         'user' => $user
+    //     ], 201);
+    // }
+
+    // // Login existing user
+    // public function login(Request $request){
+    //     $credentials = $request->validate([
+    //         'email'=>'required|email',
+    //         'password'=>'required|string'
+    //     ]);
+
+    //     if(!Auth::attempt($credentials)){
+    //         return response()->json(['message'=>'Invalid credentials'],401);
+    //     }
+
+    //     $user = Auth::user();
+    //     $token = $user->createToken('api_token')->plainTextToken;
+
+    //     return response()->json([
+    //         'user'=>$user,
+    //         'token'=>$token
+    //     ]);
+    // }
+
+    // // Logout user
+    // public function logout(Request $request){
+    //     $request->user()->tokens()->delete();
+    //     return response()->json(['message'=>'Logged out']);
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    return response()->json(['message' => 'Hello World']);
+});
+
