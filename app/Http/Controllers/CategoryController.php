20f8d4d48e4d085c5ad8dd774f11779a0c17e78a
Sizzad-Hosen <sizzadhosenofficial@gmail.com>
2025-12-14 14:31:43 +0600

    category api create,get and singel get

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+
+    //  Create category (admin only)
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+     
+        if ($request->user()->role !== 'admin') {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        $category = Category::create($request->only('name','description'));
+
+       return response()->json([
+        'message' => 'Category created successfully',
+        'category' => $category
+    ], 201);
+         
+    }
+
+   // List all categories
+    public function index()
+    {
+        $AllCategory = Category::all();
+
+        return response()->json($AllCategory);
+    }
+
+    // Show single category
+    public function show($id)
+    {
+        $category = Category::find($id);
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+        return response()->json($category);
+    }
+
+    public function destroy(){
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -8,8 +9,17 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
+Route::middleware('auth:sanctum')->group(function () {
 
+    
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
 
-Route::get('/test', function () {
-    return 'hello world';
+   
+    Route::post('/categories', [CategoryController::class, 'store']);
+
 });
+
+// Route::get('/test', function () {
+//     return 'hello world';
+// });
